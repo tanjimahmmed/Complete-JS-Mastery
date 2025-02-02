ab0ece708810a20e56d5acbd4748aa23c86e6fb0
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-02 13:09:21 +0600

    objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -476,3 +476,16 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, totals);
 */
+
+/*
+// Objects
+const tanjim = {
+  firstName: "Tanjim",
+  lastName: "Miller",
+  age: 2047 - 1998,
+  job: "programmer",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(tanjim);
+*/
