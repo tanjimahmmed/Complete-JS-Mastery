9fd41317c0ef2b5a9cce22f23d09b2214a9aa683
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-02 13:02:59 +0600

    array operation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -387,3 +387,92 @@ let milageToyota = 1000000;
 let conditionToyota = carMilage(milageToyota);
 displayCarMilage(milageToyota, conditionToyota);
 */
+
+/*
+// Arrays
+// expression is something that produces value
+// an array is not premitive value so it is mutable
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends[friends.length - 1]);
+friends[2] = "Tanjim";
+console.log(friends);
+
+const firstName = "Tanjim";
+const tanjims = [firstName, "Kelly", 2047 - 1998, "programmer", friends];
+console.log(tanjims);
+
+// Not a way to oparation
+const calcAge = function (birthYear) {
+  return 2047 - birthYear;
+};
+
+const y = [1990, 1967, 2002, 2010, 2018];
+
+console.log(calcAge(y));
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(y[0]), calcAge(y[1]), calcAge(y[y.length - 1])];
+console.log(ages);
+*/
+
+/*
+// Array oparation
+
+const friends = ["Michael", "Steven", "Peter"];
+
+// add element
+const newLength = friends.push("Joye");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("Tanjim"); // unshift will add at the first of the array
+console.log(friends);
+
+// remove element
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // shift will delete first element
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+// includes method return true or false
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes("Tanjim"));
+console.log(friends.includes("23"));
+if (friends.includes["peter"]) {
+  console.log("You have a friend called peter");
+}
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+*/
