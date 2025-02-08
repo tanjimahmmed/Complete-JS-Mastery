6b571ab2fdb74e38c73e25488c1d587f33aa28fd
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-08 16:42:10 +0600

    modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModel = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+for(let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener("click", openModal);
+
+    btnCloseModal.addEventListener("click", closeModel)
+    overlay.addEventListener("click", closeModel)
+
+document.addEventListener("keydown", function(e){
+    // console.log("A key was pressed");
+    // console.log(e);
+    if(e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModel();
+    }
+})
