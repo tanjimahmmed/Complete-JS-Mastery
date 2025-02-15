0726840774fc56c302124b4d64c1b9d50aac8350
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-15 11:49:11 +0600

    add destructuring objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/*
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -45,9 +45,55 @@ const restaurant = {
       close: 24,
     },
   },
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  // },
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
-*/
 
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Gulshan 2, Dhaka",
+  mainIndex: 1,
+  starterIndex: 2
+});
+
+restaurant.orderDelivery({
+  address: "Gulshan 1, Dhaka",
+  starterIndex: 1
+});
+
+
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// default values
+const {menu = [], starterMenu: starters = []} = restaurant;
+
+console.log(menu, starters);
+
+// mutating variables
+let a = 111;
+let b = 999;
+
+const obj = {a: 2, b : 5, c: 9};
+({a, b} = obj);
+console.log(a, b);
+
+// nested objects
+const {fri: {open: o, close: c}} = openingHours;
+// console.log(open, close);
+console.log(o, c);
+
+
+/*
+// destructure array
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -111,5 +157,7 @@ console.log(i, j, k);
 // default values
 const [p = 1, q = 1, r = 1] = [8, 9] // 1 will be added as default value
 console.log(p, q, r);
+*/
+
 
 
