fe7c4a6ce6e383fd03b864a0a08c65faccf148cb
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-15 10:18:29 +0600

    Data structure

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -45,3 +46,70 @@ const restaurant = {
     },
   },
 };
+*/
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
+};
+
+const arr = [2, 3, 5];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+console.log(x,y,z);
+console.log(arr);
+
+
+// console.log(a);
+// console.log(c);
+
+// const [first, second] = restaurant.categories;
+// const [first, ,second] = restaurant.categories;
+
+// console.log(first, second);
+
+let [main, , secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+// switching variables
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+
+// const [i, j] = nested; //this gives the first two index
+// const [i, , j] = nested; // this will return nested index
+
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+
+console.log(i, j, k);
+
+// default values
+const [p = 1, q = 1, r = 1] = [8, 9] // 1 will be added as default value
+console.log(p, q, r);
+
+
