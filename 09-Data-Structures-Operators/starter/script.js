ecf3560698f7ae73b167d65f4f2da8960f7ba115
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-02-24 19:15:51 +0600

    es6 enhanced version

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -15,6 +15,21 @@
 //     close: 24,
 //   },
 // };
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -22,38 +37,31 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhanced object literals
+  openingHours,
+  
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
   },
 
-  orderDelivery: function ({starterIndex = 1, mainIndex = 0, address, time = '20:00'}){
+  orderDelivery({starterIndex = 1, mainIndex = 0, address, time = '20:00'}){
     console.log(`Order revived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
   },
 
-  orderPasta: function(ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function(mainIngredient, ...otherIngredients){
+  orderPizza(mainIngredient, ...otherIngredients){
     console.log(mainIngredient);
     console.log(otherIngredients);
   }
 };
+
+// ----------------------------------------------------------
+
+
 
 /*
 // ----------------------------------------------------------
