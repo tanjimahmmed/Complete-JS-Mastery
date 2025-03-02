97a8a94b0287704c9b6def39582df91c646236a9
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-03-02 13:39:17 +0600

    Passing arguments - value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+/*
+// -- function default parameters
 const bookings = [];
 
 const createBooking = function (flightNumber, numPassengers = 1, price = 199 * numPassengers) {
@@ -21,3 +22,38 @@ createBooking('LH123', 2)
 createBooking('LH123', 5)
 
 createBooking('LH123', undefined, 1000)
+*/
+
+// Passing arguments - value vs reference
+const flight = 'LH324';
+const tanjim = {
+    name: 'Tanjim Emmeet',
+    passport: 245689033123
+}
+
+const checkIn = function (flightNumber, passenger) {
+    flightNumber = 'LH325';
+    passenger.name = 'Mr.' + passenger.name;
+
+    if(passenger.passport === 245689033123){
+        alert('Checked in')
+    }else {
+        alert('Wrong passport')
+    }
+}
+
+// checkIn(flight, tanjim);
+// console.log(flight);
+// console.log(tanjim);
+
+// Is the same as doing
+// const flightNumber = flight;
+// const passenger = tanjim;
+// console.log(flightNumber, passenger);
+
+const newPassport = function (person){
+    person.passport = Math.trunc(Math.random() * 10000000000000);
+}
+
+newPassport(tanjim);
+checkIn(flight, tanjim);
