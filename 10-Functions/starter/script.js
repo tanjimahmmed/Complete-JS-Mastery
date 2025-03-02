4f415eb7209f703ae142d1db9e46bdc73da03e40
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-03-02 12:34:47 +0600

    function default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNumber, numPassengers = 1, price = 199 * numPassengers) {
+    // ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+    const booking = {
+        flightNumber,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+
+    bookings.push(booking);
+}
+createBooking('LH123')
+createBooking('LH123', 2, 100)
+createBooking('LH123', 2)
+createBooking('LH123', 5)
+
+createBooking('LH123', undefined, 1000)
