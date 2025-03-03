d59e705553ecbe075843be9c063058a0c738a860
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-03-03 13:24:13 +0600

    add Function returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,6 +24,7 @@ createBooking('LH123', 5)
 createBooking('LH123', undefined, 1000)
 */
 
+/*
 // Passing arguments - value vs reference
 const flight = 'LH324';
 const tanjim = {
@@ -57,3 +58,61 @@ const newPassport = function (person){
 
 newPassport(tanjim);
 checkIn(flight, tanjim);
+*/
+
+/*
+// function accepting callback function
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function(str, fn){
+    console.log(`Original string: ${str}`);
+    
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+    
+    
+}
+transformer('JavaScript is...', upperFirstWord)
+transformer('JavaScript is...', oneWord)
+
+// js uses callbacks all the time
+const high5 = function() {
+    console.log('👋');
+}
+
+document.body.addEventListener('click', high5); //callback function
+
+['tanjim', 'jahid', 'megnum'].forEach(high5)
+*/
+
+// Function returning functions
+// const greet = function(greeting){
+//     return function(name){
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
+
+// const greeterHey = greet('Hey');
+// greeterHey('Tanjim')
+// greeterHey('Steve')
+// greet('Hello')('Tanjim')
+
+// const greet = (greeting) => {
+//     return (name) => {
+//         console.log(`${greeting} ${name}`)
+//     }
+// }
+const greet = greeting => name => console.log(`${greeting} ${name}`) // single line arrow func
+const greeterHey = greet('Hey');
+greeterHey('Tanjim')
+greeterHey('Steve')
+greet('Hello')('Tanjim')
